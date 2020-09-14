96e19ebfb9f5386ad8cdc8c0a709ea3f043be2b7
Arthur Dorozhkin <arthur_dorozhkin@epam.com>
2020-09-14 17:13:26 +0300

    Add javadoc to CSVParser

--- first_project/src/main/java/com/epam/big_data/converters/csv/CSVParser.java
+++ first_project/src/main/java/com/epam/big_data/converters/csv/CSVParser.java
@@ -10,10 +10,21 @@ import java.io.Reader;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Class for parsing CSV files
+ *
+ * @author Arthur_Dorozhkin
+ * @version 1.0
+ */
 public class CSVParser {
 
      private final static Logger LOGGER = LogManager.getLogger();
 
+     /**
+      * Method that gets the <code>reader</code> and read CSV data from it to list of strings arrays
+      * @param reader Source of CSV data
+      * @return List of strings arrays with parsed data
+      */
      public static List<String[]> readAll(Reader reader) {
           LOGGER.info("Starts reading the CSV file");
           try (CSVReader csvReader = new CSVReader(reader)) {
