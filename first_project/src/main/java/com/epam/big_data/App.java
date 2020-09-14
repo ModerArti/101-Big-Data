91efd0ded61d2837997e7cb2ccd2354404e1175f
Arthur Dorozhkin <arthur_dorozhkin@epam.com>
2020-09-14 17:04:57 +0300

    Add using the AVRO parser in main

--- first_project/src/main/java/com/epam/big_data/App.java
+++ first_project/src/main/java/com/epam/big_data/App.java
@@ -1,14 +1,14 @@
 package com.epam.big_data;
 
+import com.epam.big_data.converters.avro.AVROParser;
 import com.epam.big_data.converters.csv.CSVParser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.io.Writer;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.LinkedList;
 import java.util.List;
 
 public class App {
@@ -16,33 +16,28 @@ public class App {
     private static final Logger LOGGER = LogManager.getLogger();
 
     public static void main(String[] args) {
-        try {
-            List<String[]> strings = readAll();
-            writeAll(strings);
+        List<String[]> strings = readAllFromCSV();
+        writeAllToAVRO(strings);
+    }
+
+    private static List<String[]> readAllFromCSV() {
+        try (Reader reader = Files.newBufferedReader(Paths.get(
+                "first_project/files/csv/sample_submission.csv"
+        ))) {
+            return CSVParser.readAll(reader);
         } catch (IOException e) {
             LOGGER.error(e);
         }
+        return new LinkedList<>();
     }
 
-    private static List<String[]> readAll() throws IOException {
-        Reader reader = Files.newBufferedReader(Paths.get(
-                "first_project/files/csv/sample_submission.csv"
-        ));
-        return CSVParser.readAll(reader);
-    }
-
-    private static void writeAll(List<String[]> strings) throws IOException {
-        Writer writer = Files.newBufferedWriter(Paths.get(
-                "first_project/files/csv/sample_submission.txt"
-        ));
-        for (String[] stringArray : strings) {
-            for (int i = 0; i < stringArray.length; ++i) {
-                writer.write(stringArray[i]);
-                if (i != stringArray.length - 1) {
-                    writer.write(", ");
-                }
-            }
-            writer.write("\n");
+    private static void writeAllToAVRO(List<String[]> strings) {
+        try (OutputStream output = new FileOutputStream(new File("first_project/files/avro/data.avro"))) {
+            AVROParser.writeAll(strings, output);
+        } catch (FileNotFoundException e) {
+            LOGGER.error("File not found", e);
+        } catch (IOException e) {
+            LOGGER.error("Some problem with writing", e);
         }
     }
 
