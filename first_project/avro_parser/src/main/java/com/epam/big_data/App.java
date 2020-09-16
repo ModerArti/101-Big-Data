98b28a52811e5bf3d76e64817f5e754be2bdb3be
Arthur Dorozhkin <arthur_dorozhkin@epam.com>
2020-09-16 21:28:35 +0300

    Add using HDFSConnector in main

--- first_project/avro_parser/src/main/java/com/epam/big_data/App.java
+++ first_project/avro_parser/src/main/java/com/epam/big_data/App.java
@@ -1,14 +1,14 @@
 package com.epam.big_data;
 
-import com.epam.big_data.config.Config;
 import com.epam.big_data.converters.avro.AVROParser;
 import com.epam.big_data.converters.csv.CSVParser;
+import com.epam.big_data.hdfs.HDFSConnector;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -22,32 +22,26 @@ public class App {
 
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public static void main(String[] args) {
-        List<String[]> strings = readAllFromCSV();
-        writeAllToAVRO(strings);
+    public static void main(String[] args) throws IOException {
+        PipedOutputStream outFromHDFStoCSV = HDFSConnector.readFile();
+        List<String[]> strings = readAllFromCSV(outFromHDFStoCSV);
+        PipedOutputStream outFromAVROToHDFS = writeAllToAVRO(strings);
+        HDFSConnector.writeFile(new PipedInputStream(outFromAVROToHDFS));
     }
 
-    private static List<String[]> readAllFromCSV() {
-        try (Reader reader = Files.newBufferedReader(Paths.get(
-                Config.loadProperty("file.csv")
-        ))) {
-            return CSVParser.readAll(reader);
+    private static List<String[]> readAllFromCSV(PipedOutputStream out) {
+        try (PipedInputStream in = new PipedInputStream(out)) {
+            return CSVParser.readAll(in);
         } catch (IOException e) {
             LOGGER.error(e);
         }
         return new LinkedList<>();
     }
 
-    private static void writeAllToAVRO(List<String[]> strings) {
-        try (OutputStream output = new FileOutputStream(new File(
-                Config.loadProperty("file.avro")
-        ))) {
-            AVROParser.writeAll(strings, output);
-        } catch (FileNotFoundException e) {
-            LOGGER.error("File not found", e);
-        } catch (IOException e) {
-            LOGGER.error("Some problem with writing", e);
-        }
+    private static PipedOutputStream writeAllToAVRO(List<String[]> strings) {
+        PipedOutputStream output = new PipedOutputStream();
+        AVROParser.writeAll(strings, output);
+        return output;
     }
 
 }
