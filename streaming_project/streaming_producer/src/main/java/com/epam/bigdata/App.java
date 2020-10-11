ee20ad1ac95238bac65347eeb6257edd0f052034
Arthur Dorozhkin <arthur_dorozhkin@epam.com>
2020-10-11 23:09:35 +0300

    Add smart producing of data to kafka

--- streaming_project/streaming_producer/src/main/java/com/epam/bigdata/App.java
+++ streaming_project/streaming_producer/src/main/java/com/epam/bigdata/App.java
@@ -1,26 +1,47 @@
 package com.epam.bigdata;
 
+import com.epam.bigdata.model.Hotel;
+import com.epam.bigdata.serializer.JavaSerializer;
+import com.epam.bigdata.sparkhandler.RDDHandler;
 import org.apache.kafka.clients.producer.*;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 
+import java.util.List;
 import java.util.Properties;
 
 public class App {
 
-    public static void main(String[] args){
-
+    private static Properties getKafkaProps() {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.ACKS_CONFIG, "all");
         props.put(ProducerConfig.RETRIES_CONFIG, 0);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JavaSerializer.class.getName());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        return props;
+    }
 
-        Producer<String, String> producer = new KafkaProducer<>(props);
+    private static JavaSparkContext getSparkContext() {
+        SparkConf conf = new SparkConf().setAppName("Producer").setMaster("local[*]");
+        return new JavaSparkContext(conf);
+    }
+
+    public static void main(String[] args){
+        Producer<String, Hotel> producer = new KafkaProducer<>(getKafkaProps());
+
+        JavaSparkContext sc = getSparkContext();
+
+        RDDHandler rddHandler = new RDDHandler(sc);
+
+        List<Hotel> hotels = rddHandler.getData("test.csv");
+
         TestCallback callback = new TestCallback();
-        for (long i = 0; i < 100 ; i++) {
-            ProducerRecord<String, String> data = new ProducerRecord<>(
-                    "homework", "key-" + i, "message-" + i);
+        for (Hotel hotel : hotels) {
+            ProducerRecord<String, Hotel> data = new ProducerRecord<>(
+                    "homework", hotel);
             producer.send(data, callback);
+            producer.flush();
         }
 
         producer.close();
