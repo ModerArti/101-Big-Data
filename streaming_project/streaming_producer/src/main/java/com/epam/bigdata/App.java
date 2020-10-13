eff7f5e9e6eb0e1ac83477777ea3ad2fee889a3d
Arthur Dorozhkin <arthur_dorozhkin@epam.com>
2020-10-13 17:13:10 +0300

    Remove RDDHandler

--- streaming_project/streaming_producer/src/main/java/com/epam/bigdata/App.java
+++ streaming_project/streaming_producer/src/main/java/com/epam/bigdata/App.java
@@ -2,13 +2,14 @@ package com.epam.bigdata;
 
 import com.epam.bigdata.model.Hotel;
 import com.epam.bigdata.serializer.JavaSerializer;
-import com.epam.bigdata.sparkhandler.RDDHandler;
-import org.apache.kafka.clients.producer.*;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
-import java.util.List;
 import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class App {
 
@@ -22,42 +23,29 @@ public class App {
         return props;
     }
 
-    private static JavaSparkContext getSparkContext() {
-        SparkConf conf = new SparkConf().setAppName("Producer").setMaster("local[*]");
-        return new JavaSparkContext(conf);
+    private static Hotel generateHotels() {
+        Random random = new Random();
+        Hotel hotel = new Hotel(
+                Integer.toString(random.nextInt(100)),
+                Integer.toString(random.nextInt(100)),
+                Integer.toString(random.nextInt(100)),
+                Integer.toString(random.nextInt(100))
+        );
+        return hotel;
     }
 
-    public static void main(String[] args){
-        Producer<String, Hotel> producer = new KafkaProducer<>(getKafkaProps());
-
-        JavaSparkContext sc = getSparkContext();
-
-        RDDHandler rddHandler = new RDDHandler(sc);
-
-        List<Hotel> hotels = rddHandler.getData("test.csv");
-
-        TestCallback callback = new TestCallback();
-        for (Hotel hotel : hotels) {
-            ProducerRecord<String, Hotel> data = new ProducerRecord<>(
-                    "homework", hotel);
-            producer.send(data, callback);
-            producer.flush();
-        }
-
-        producer.close();
-    }
-
-
-    private static class TestCallback implements Callback {
-        @Override
-        public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-            if (e != null) {
-                System.out.println("Error while producing message to topic :" + recordMetadata);
-                e.printStackTrace();
-            } else {
-                String message = String.format("sent message to topic:%s partition:%s  offset:%s", recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset());
-                System.out.println(message);
+    public static void main(String[] args) {
+        try (Producer<String, Hotel> producer = new KafkaProducer<>(getKafkaProps())) {
+            while (true) {
+                Hotel hotel = generateHotels();
+                ProducerRecord<String, Hotel> data = new ProducerRecord<>(
+                        "homework", hotel);
+                producer.send(data);
+                producer.flush();
+                Thread.sleep(TimeUnit.SECONDS.toMillis(5));
             }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
